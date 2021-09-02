2e705ceb8b2b9cda7d30e4692a16d2e181eded0b
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2021-09-02 08:16:27 -0300

    create and mapping the entities

---
+++ src/main/java/com/api/person/entity/Person.java
@@ -0,0 +1,38 @@
+package com.api.person.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    // the cascade purpose is to insert a person and its phone without persist the phone previously
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Phone> phones;
+}
