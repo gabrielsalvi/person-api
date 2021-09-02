ba16d044573ece734f2942d666cc12a5bfe06dc1
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2021-09-02 09:12:56 -0300

    fix some entity classes attributes

--- src/main/java/com/api/person/entity/Person.java
+++ src/main/java/com/api/person/entity/Person.java
@@ -5,13 +5,21 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Builder
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person
@@ -29,10 +37,9 @@ public class Person
     @Column(nullable = false, unique = true)
     private String cpf;
 
-    @Column(nullable = false)
     private LocalDate birthDate;
 
     // the cascade purpose is to insert a person and its phone without persist the phone previously
     @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
-    private List<Phone> phones;
+    private List<Phone> phones = new ArrayList<>();
 }
